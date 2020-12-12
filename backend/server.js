265f57d9cb3303d040d867873b6480f8614b89af
simxn1 <75263268+simxn1@users.noreply.github.com>
2020-12-12 16:31:11 +0100

    changed port variable

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express()
-const port = 5000
+const port = process.env.NODE_PORT || 80
 
 app.use(cors())
 app.use(express.json())
