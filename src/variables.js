9b2f559312c4e1e9e3d77834e4589c05e1e3f483
simxn1 <mcmunka@gmail.com>
2020-12-20 00:32:44 +0100

    changed url

--- src/variables.js
+++ src/variables.js
@@ -2,7 +2,7 @@ const PRODUCTION_MODE = process.env.NODE_ENV === "production";
 
 let SERVER = "http://localhost:8080";
 if (PRODUCTION_MODE) {
-  SERVER = "clients";
+  SERVER = "";
   console.log("prod", SERVER);
 } else {
   console.log("not prod", SERVER);
