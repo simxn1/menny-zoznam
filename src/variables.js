9c6b48c4f67ebad67fa5c4a995135adbd542858e
simxn1 <mcmunka@gmail.com>
2020-12-21 21:10:04 +0100

    added clients to url

--- src/variables.js
+++ src/variables.js
@@ -2,7 +2,7 @@ const PRODUCTION_MODE = process.env.NODE_ENV === "production";
 
 let SERVER = "http://localhost:8080";
 if (PRODUCTION_MODE) {
-  SERVER = "";
+  SERVER = "clients";
   console.log("prod", SERVER);
 } else {
   console.log("not prod", SERVER);
